59569cc0ed91dc2cbe5c5e8eda9284014067d71a
Kozim <kozim_7484443@mail.ru>
2023-08-22 17:54:22 +0500

    Вёрстка завершена

--- app/src/js/common.js
+++ app/src/js/common.js
@@ -8,3 +8,25 @@ const swiper = new Swiper(".swiper", {
         disableOnInteraction: false,
     },
 });
+
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function (element) {
+    element.addEventListener('click', open);
+});
+
+function open(e) {
+    const tabTarget = e.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach(function (item) {
+        item.classList.remove('tabs__btn-item_active');
+    })
+    tabTarget.classList.add('tabs__btn-item_active');
+
+    tabContent.forEach(function(item) {
+        item.classList.remove('tabs__content-item_active');
+    });
+    document.querySelector(`#${button}`).classList.add('tabs__content-item_active');
+}
